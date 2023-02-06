6c672aea5be2ee5dff3a47377084928dc3b60c3b
Rafael Mariano da Silva <77937182+RafaelSilva-si@users.noreply.github.com>
2023-02-06 06:57:45 -0300

    Fix: adjuste object questions returno

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,21 +1,24 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { Quiz } from './entities/quiz.entity';
+import { shuffle } from 'src/Helpers/functions';
 
 @Controller('questions')
 export class QuizController {
   constructor(private readonly quizService: QuizService) {}
 
-  @Get(':quizId')
-  async findOne(@Param('quizId') id: string) {
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
     let response = await this.quizService.findAll(+id);
     response = response.data.map((quiz: Quiz) => ({
       ...quiz,
       category: decodeURIComponent(quiz.category),
       question: decodeURIComponent(quiz.question),
       correct_answer: decodeURIComponent(quiz.correct_answer),
-      incorrect_answers: quiz.incorrect_answers.map((ans) =>
-        decodeURIComponent(ans),
+      incorrect_answers: shuffle(
+        quiz.incorrect_answers
+          .map((ans) => decodeURIComponent(ans))
+          .concat(decodeURIComponent(quiz.correct_answer)),
       ),
     }));
     return response;
